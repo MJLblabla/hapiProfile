28cfecdf6d3c634f1e2e85ffbeaf7ac93837e35a
manjiale <1013666753@qq.com>
2020-07-02 16:41:28 +0800

    temp

--- hapiaop/src/main/java/com/hapi/aop/HapiMonitorPlugin.kt
+++ hapiaop/src/main/java/com/hapi/aop/HapiMonitorPlugin.kt
@@ -51,58 +51,65 @@ object HapiMonitorPlugin {
                 GlobalScope.launch(Dispatchers.Main) {
 
                    val res= async< LinkedList<Beat> ?> {
+
+
                         val beatsClone =
                             LinkedList<Beat>()
-                        var maxCost = 0
-                        var i = beats.size - 1
-                        while (i < 0) {
-                            beatsClone.add(beats[i])
-                            if (beats[i].cost > maxCost) {
-                                maxCost = beats[i].cost
-                            }
-                            i--
-                        }
+                       if(beats.isEmpty()){
+                            Log.d("mjl","beats is empty")
+                       }else{
+                           var maxCost = 0
+                           var i = beats.size - 1
+                           while (i < 0) {
+                               beatsClone.add(beats[i])
+                               if (beats[i].cost > maxCost) {
+                                   maxCost = beats[i].cost
+                               }
+                               i--
+                           }
 
-                        if (MethodBeatMonitorJava.mBeatAdapter == null ||
-                            beatsClone.isEmpty()
-                            ||maxCost < maxTop *0.5
-                                ) {
-                            Log.d("mjl","maxCost"+maxCost)
-                        }else{
-                            var methodName = ""
-                            var cost = 0
-                            val iterator =
-                                beatsClone.iterator()
-                            val beatTemp =
-                                LinkedList<Beat>()
+                           if (MethodBeatMonitorJava.mBeatAdapter == null ||
+                               beatsClone.isEmpty()
+                               ||maxCost < maxTop *0.5
+                           ) {
+                               Log.d("mjl","maxCost"+maxCost)
+                           }else{
+                               var methodName = ""
+                               var cost = 0
+                               val iterator =
+                                   beatsClone.iterator()
+                               val beatTemp =
+                                   LinkedList<Beat>()
 
-                            while (iterator.hasNext()) {
-                                val i = iterator.next()
-                                val index = i.sign.lastIndexOf('(')
-                                if (index <= 0) {
-                                    continue
-                                }
-                                val itemMethodNameArrayButParam2 = i.sign.substring(0, index)
-                                val itemMethodNameArrayParam = i.sign.substring(index)
-                                val itemMethodNameArray =
-                                    itemMethodNameArrayButParam2.split("\\.".toRegex()).toTypedArray()
-                                if (itemMethodNameArray.size < 2) {
-                                    continue
-                                }
-                                val name =
-                                    itemMethodNameArray[itemMethodNameArray.size - 1] + itemMethodNameArrayParam
-                                if (name == methodName && i.cost == cost) {
-                                    iterator.remove()
-                                } else {
-                                    beatTemp.add(i)
-                                    if (beatTemp.size > issureTop) {
-                                        break
-                                    }
-                                }
-                                methodName = name
-                                cost = i.cost
-                            }
-                        }
+                               while (iterator.hasNext()) {
+                                   val i = iterator.next()
+                                   val index = i.sign.lastIndexOf('(')
+                                   if (index <= 0) {
+                                       continue
+                                   }
+                                   val itemMethodNameArrayButParam2 = i.sign.substring(0, index)
+                                   val itemMethodNameArrayParam = i.sign.substring(index)
+                                   val itemMethodNameArray =
+                                       itemMethodNameArrayButParam2.split("\\.".toRegex()).toTypedArray()
+                                   if (itemMethodNameArray.size < 2) {
+                                       continue
+                                   }
+                                   val name =
+                                       itemMethodNameArray[itemMethodNameArray.size - 1] + itemMethodNameArrayParam
+                                   if (name == methodName && i.cost == cost) {
+                                       iterator.remove()
+                                   } else {
+                                       beatTemp.add(i)
+                                       if (beatTemp.size > issureTop) {
+                                           break
+                                       }
+                                   }
+                                   methodName = name
+                                   cost = i.cost
+                               }
+                           }
+                       }
+
 
                        beatsClone
                     }
